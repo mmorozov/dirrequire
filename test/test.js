f34ec81445d945db8719c4e635ef755c09b34ded
Михаил Морозов <morozov@serenity.su>
2014-01-06 15:03:19 +0400

    tests

---
+++ test/test.js
@@ -0,0 +1,77 @@
+var dirrequire = require('../lib/main');
+var co         = require('co');
+
+describe('Testing first mock', function() {
+  var test = {
+    err: null,
+    result: null,
+    load: false
+  };
+
+  before(function(done) {
+    dirrequire(__dirname + "/mocks/1", function(err, result) {
+      test.error = err;
+      test.result = result;
+      test.load = true;
+
+      done();
+    });
+  });
+
+  it('mock should be load', function(done) {
+    test.load.should.be.true;
+    done();
+  });
+
+  it('error should be empty', function(done) {
+    (test.error === null).should.be.true;
+    done();
+  });
+
+  it('testing result', function(done) {
+    test.result.should.have.property('foo');
+    (test.result.foo() === 'foo').should.be.true;
+
+    test.result.should.have.property('load');
+    (test.result.load() === 'load').should.be.true;
+    done();
+  });
+});
+
+describe('Testing second mock', function() {
+  var test = {
+    err: null,
+    result: null,
+    load: false
+  };
+
+  before(function(done) {
+    dirrequire(__dirname + "/mocks/2", ['js', 'json'], function(err, result) {
+      test.error = err;
+      test.result = result;
+      test.load = true;
+
+      done();
+    });
+  });
+
+  it('mock should be load', function(done) {
+    test.load.should.be.true;
+    done();
+  });
+
+  it('error should be empty', function(done) {
+    (test.error === null).should.be.true;
+    done();
+  });
+
+  it('testing result', function(done) {
+    test.result.should.have.property('foo');
+    test.result.should.have.property('load');
+    test.result.should.have.property('conf');
+    test.result.should.not.have.property('cappuccino');
+    test.result.conf.should.have.property('foo');
+
+    done();
+  });
+});
